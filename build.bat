54660fd38b8a43f06bacdf68cf5711c977e747f8
Wojciech A. Koszek <wojciech@koszek.com>
2015-08-13 23:48:41 -0700

    Print some debugging.

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 SET PATH=C:\Qt\5.5\mingw492_32\bin;%PATH%
 SET PATH=C:\Qt\Tools\mingw492_32\bin;%PATH%
+ECHO Running QMAKE
 qmake.exe -makefile -win32 FLViz.pro
+ECHO Running the rest of Makes
 mingw32-make.exe clean
 mingw32-make.exe mocclean
 mingw32-make.exe
