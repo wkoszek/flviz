94aa02bc854226574396be87278b6be3f6f4e8fb
Wojciech A. Koszek <wojciech@koszek.com>
2015-08-13 23:33:33 -0700

    Makefiles are autogenerated. Remove them.

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 SET PATH=C:\Qt\5.5\mingw492_32\bin;%PATH%
 SET PATH=C:\Qt\Tools\mingw492_32\bin;%PATH%
+qmake.exe -makefile -win32 FLViz.pro
 mingw32-make.exe clean
 mingw32-make.exe mocclean
-mingw32-make.exe distclean
-qmake.exe -makefile -win32 FLViz.pro
 mingw32-make.exe
+mingw32-make.exe distclean
