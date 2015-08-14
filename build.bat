581371f38fc38faa4aaf4024382740525c1077bd
Wojciech A. Koszek <wojciech@koszek.com>
2015-08-13 22:30:47 -0700

    Add building script for easier appveyor debugging.

---
+++ build.bat
@@ -0,0 +1,7 @@
+SET PATH=C:\Qt\5.5\mingw492_32\bin;%PATH%
+SET PATH=C:\Qt\Tools\mingw492_32\bin;%PATH%
+mingw32-make.exe clean
+mingw32-make.exe mocclean
+mingw32-make.exe distclean
+qmake.exe -makefile -win32 FLViz.pro
+mingw32-make.exe
